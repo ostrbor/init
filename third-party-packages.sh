25f967b437699681a18fd76bf52f18a0b77b9000
ostrbor <ostrbor@gmail.com>
2017-03-05 09:24:54 +0300

    Move docker to separate dir

--- third-party-packages.sh
+++ third-party-packages.sh
@@ -28,20 +28,3 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # spacemacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-
-# Docker
-sudo apt-get -y install apt-transport-https ca-certificates
-sudo apt-key adv \
-     --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-     --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt-get -y install docker-engine
-sudo service docker start
-sudo groupadd docker
-sudo usermod -aG docker $USER
-
-# docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
