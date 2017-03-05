4f4494ac3d7f6f3840400e6e5a01de7d26096e32
ostrbor <ostrbor@gmail.com>
2017-03-05 08:58:28 +0300

    Add nvidia drivers

--- third-party-packages.sh
+++ third-party-packages.sh
@@ -8,6 +8,7 @@ sudo add-apt-repository -y ppa:aacebedo/fasd
 sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner" # skype
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo add-apt-repository -y ppa:linuxgndu/sqlitebrowser
+sudo add-apt-repository -y ppa:graphics-drivers/ppa
 
 # Update
 sudo apt-get update
@@ -20,6 +21,7 @@ sudo apt-get -y install fasd
 sudo apt-get -y install skype
 sudo apt-get -y install ansible
 sudo apt-get -y install sqlitebrowser
+sudo apt-get -y install nvidia-375 # check for latest version
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
